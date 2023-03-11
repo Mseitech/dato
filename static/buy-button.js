19a724f00e73ca0b52f80787ce05398daded73e7
Maciej Salaga <maciej.salaga@gmail.com>
2023-03-11 18:45:40 +0100

    xxx

--- static/buy-button.js
+++ static/buy-button.js
@@ -25,7 +25,7 @@
     ShopifyBuy.UI.onReady(client).then(function (ui) {
       ui.createComponent('collection', {
         id: '436091355412',
-        node: document.getElementById('collection-component-1678530978835'),
+        node: document.getElementById('collection-component-1678530978823'),
         moneyFormat: '%7B%7Bamount_no_decimals%7D%7D%20kr',
         options: {
   "product": {
